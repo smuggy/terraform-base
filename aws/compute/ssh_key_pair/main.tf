93c778507c0bbcd01677a2b6faf7c1f3766436c0
marc mckernan <marc.mckernan@gmail.com>
2021-03-27 14:47:19 -0500

    fix naming

--- aws/compute/ssh_key_pair/main.tf
+++ aws/compute/ssh_key_pair/main.tf
@@ -2,7 +2,7 @@ module key_pair {
   source = "../../../tls/key_pair"
 }
 
-resource aws_key_pair utility_pair {
+resource aws_key_pair pair {
   key_name   = var.key_name
   public_key = module.key_pair.public_key_ssh
 }
--- aws/compute/ssh_key_pair/outputs.tf
+++ aws/compute/ssh_key_pair/outputs.tf
@@ -3,13 +3,13 @@ output private_key_pem {
 }
 
 output public_key_ssh {
-  value = aws_key_pair.utility_pair.public_key
+  value = aws_key_pair.pair.public_key
 }
 
 output key_pair_id {
-  value = aws_key_pair.utility_pair.key_pair_id
+  value = aws_key_pair.pair.key_pair_id
 }
 
 output key_pair_name {
-  value = aws_key_pair.utility_pair.key_name
+  value = aws_key_pair.pair.key_name
 }
