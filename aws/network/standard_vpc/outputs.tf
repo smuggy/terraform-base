d75aea4d377c95cc0c9f302ffaaadc5311eacdda
marc mckernan <marc.mckernan@gmail.com>
2021-03-30 20:39:01 -0500

    add vpc module

--- aws/network/standard_vpc/outputs.tf
+++ aws/network/standard_vpc/outputs.tf
@@ -1,5 +1,5 @@
 output vpc_id {
-  value = aws_vpc.kubernetes.id
+  value = aws_vpc.vpc.id
 }
 
 output route_table_id {
@@ -7,13 +7,13 @@ output route_table_id {
 }
 
 output subnet_a_id {
-  value = aws_subnet.ohio_a.id
+  value = aws_subnet.az_a.id
 }
 
 output subnet_b_id {
-  value = aws_subnet.ohio_b.id
+  value = aws_subnet.az_b.id
 }
 
 output subnet_c_id {
-  value = aws_subnet.ohio_c.id
+  value = aws_subnet.az_c.id
 }
