8f68cd28c84b31ec5934ec396e2fad8cb9f8b7be
marc mckernan <marc.mckernan@gmail.com>
2021-05-14 07:13:22 -0500

    update security group stuff

--- azure/network/virtual_network/outputs.tf
+++ azure/network/virtual_network/outputs.tf
@@ -6,6 +6,18 @@ output net_id {
   value = azurerm_virtual_network.net.id
 }
 
-output subnet {
-  value = azurerm_virtual_network.net.subnet
+output subnet_1_id {
+  value = azurerm_subnet.subnet_1.id
+}
+
+output subnet_1_name {
+  value = azurerm_subnet.subnet_1.name
+}
+
+output subnet_2_id {
+  value = azurerm_subnet.subnet_2.id
+}
+
+output subnet_2_name {
+  value = azurerm_subnet.subnet_2.name
 }
